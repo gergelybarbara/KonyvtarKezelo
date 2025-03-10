da04859000b2766496ebb535d84c704b8d51361b
Futty Imre <ap@d.dd>
2025-03-10 19:48:15 +0100

    book

--- KonyvtarKezelo/Models/Book.cs
+++ KonyvtarKezelo/Models/Book.cs
@@ -8,5 +8,29 @@ namespace KonyvtarKezelo.Models
 {
     public class Book
     {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        bool isAvailable { get; set; }
+
+        public Book(string iSBN, string title, string author, int year, bool isAvailable)
+        {
+            ISBN = iSBN;
+            Title = title;
+            Author = author;
+            Year = year;
+            this.isAvailable = true;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"A könyv azonosító száma: {ISBN}, címe: {Title}, szerzője: {Author}, kiadási éve {Year} " + (isAvailable? "kölcsönözhető" : "nem kölcsönözhető"));
+        }
+
+        public override string? ToString()
+        {
+            return base.ToString();
+        }
     }
 }
